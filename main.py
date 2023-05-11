325e3aeb51cc61851419a4eacca05b88ebb79e82
Yeraldine <yyba1997@gmail.com>
2023-05-11 18:26:03 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# Open browser.
+# selenium 4
+import time
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+time.sleep(1)
+
+# Go to webpage.
+driver.get("https://practicetestautomation.com/practice-test-login/")
+
+# Type username student into Username field
+username_locator = driver.find_element(By.XPATH, "//input[@id='username']")
+username_locator.send_keys("student")
+
+# Type password Password123 into Password field
+password_locator = driver.find_element(By.XPATH, "//input[@id='password']")
+password_locator.send_keys("Password123")
+
+# Push Submit button
+submit_button_locator = driver.find_element(By.XPATH, "//button[@id='submit']")
+submit_button_locator.click()
+time.sleep(2)
+
+# Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+actual_url = driver.current_url
+assert actual_url == "https://practicetestautomation.com/logged-in-successfully/"
+
+# Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+text_locator = driver.find_element(By.XPATH, "//div[@id='loop-container']//article//h1[@class='post-title']")
+actual_text = text_locator.text
+assert actual_text == "Logged In Successfully"
+
+# Verify button Log out is displayed on the new page
+log_out_button_locator = driver.find_element(By.XPATH, "//div[@id='loop-container']/div/article//a[@href='https://practicetestautomation.com/practice-test-login/']")
+assert log_out_button_locator.is_displayed()
