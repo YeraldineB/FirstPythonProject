576570c161eb9cf2a8a4577324bf9aa2726c2b1c
Yeraldine <yyba1997@gmail.com>
2023-05-19 18:36:13 -0300

    Section 5

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,12 +2,27 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
-@pytest.fixture()
-def driver():
-    print("Creating chrome driver")
-    my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+@pytest.fixture(params=["chrome", "edge"])
+def driver(request):
+    # browser = request.config.getoption("--browser")
+    browser = request.param
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    elif browser == "edge":
+        my_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    else:
+        raise TypeError(f"Expected 'chrome' or 'edge', but got {browser}")
     yield my_driver
-    print("Closing chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="Browser to execute tests (chrome or edge)"
+    )
