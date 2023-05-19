7e8d8a845b88c01f2d25d0b6f50d7a7116c3bc82
Yeraldine <yyba1997@gmail.com>
2023-05-19 15:19:36 -0300

    Parametrized tests

--- Tests/test_login_page_negative.py
+++ Tests/test_login_page_negative.py
@@ -7,6 +7,34 @@ from selenium.webdriver.common.by import By
 class TestNegativeScenarios:
     @pytest.mark.login
     @pytest.mark.negative
+    @pytest.mark.parametrize("username, password, expected_error_message",
+                             [("incorrectUser", "Password123", "Your username is invalid!"),
+                              ("student", "incorrectPassword", "Your password is invalid!")])
+    def test_negative_login(self, driver, username, password, expected_error_message):
+        # Open page
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # Type username incorrectUser into Username field
+        username_locator = driver.find_element(By.XPATH, "//input[@id='username']")
+        username_locator.send_keys(username)
+
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.XPATH, "//input[@id='password']")
+        password_locator.send_keys(password)
+
+        # Push Submit button
+        submit_button_locator = driver.find_element(By.XPATH, "//button[@id='submit']")
+        submit_button_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.XPATH, "//div[@id='error']")
+        assert error_message_locator.is_displayed(), "Error message is not displayed but, it should be"
+
+        # Verify error message text is Your username is invalid!
+        error_message = error_message_locator.text
+        assert error_message == expected_error_message
+
     def test_negative_username(self, driver):
         # Open page
         driver.get("https://practicetestautomation.com/practice-test-login/")
@@ -32,8 +60,6 @@ class TestNegativeScenarios:
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!"
 
-    @pytest.mark.login
-    @pytest.mark.negative
     def test_negative_password(self, driver):
         # Open page
         driver.get("https://practicetestautomation.com/practice-test-login/")
